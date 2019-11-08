e97662a4c97069dcaf142f02ff678fd42ff6363b
Raffael Martins <raffael.martins@receita.pb.gov.br>
2019-11-08 11:52:20 -0300

    Configurar sequelizerc

---
+++ src/database/index.js
@@ -0,0 +1,8 @@
+import 'dotenv/config';
+
+import Sequelize from 'sequelize';
+import dbConfig from '../config/database';
+
+const connection = new Sequelize(dbConfig);
+
+export default connection;
