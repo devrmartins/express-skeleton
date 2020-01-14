955202f308ec6b08985eefc1b8c39235f4c02af3
Devrmartins Server <devrmartins@gmail.com>
2020-01-14 11:38:34 -0500

    Adicionar configuracao da porta do banco

--- src/config/database.js
+++ src/config/database.js
@@ -4,6 +4,7 @@ module.exports = {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
     define: {
         timestamps: true,
         underscored: true
